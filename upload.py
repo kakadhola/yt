a18a762729d11c2e267462e5da7d4e7f6293a535
kakadhola <99184447+kakadhola@users.noreply.github.com>
2023-03-24 16:18:38 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import argparse
-import httplib
+import http.client
 import httplib2
 import os
 import random
@@ -20,15 +20,15 @@ httplib2.RETRIES = 1
 
 MAX_RETRIES = 10
 
-RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
-  httplib.IncompleteRead, httplib.ImproperConnectionState,
-  httplib.CannotSendRequest, httplib.CannotSendHeader,
-  httplib.ResponseNotReady, httplib.BadStatusLine)
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnected,
+  http.client.IncompleteRead, http.client.ImproperConnectionState,
+  http.client.CannotSendRequest, http.client.CannotSendHeader,
+  http.client.ResponseNotReady, http.client.BadStatusLine)
 
 
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
-CLIENT_SECRETS_FILE = raw_input('Enter your client credential secret file path:\n')
+CLIENT_SECRETS_FILE = input('Enter your client credential secret file path:\n')
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 API_SERVICE_NAME = 'youtube'
@@ -61,7 +61,7 @@ def initialize_upload(youtube, options, MEDIA_FILE_PATH):
   )
 
   insert_request = youtube.videos().insert(
-    part=','.join(body.keys()),
+    part=','.join(list(body.keys())),
     body=body,
 
     media_body=MediaFileUpload(MEDIA_FILE_PATH, chunksize=-1, resumable=True)
@@ -75,37 +75,37 @@ def resumable_upload(request):
   retry = 0
   while response is None:
     try:
-      print 'Uploading file...'
+      print('Uploading file...')
       status, response = request.next_chunk()
       if response is not None:
         if 'id' in response:
-          print 'Video id "%s" was successfully uploaded.' % response['id']
+          print('Video id "%s" was successfully uploaded.' % response['id'])
         else:
           exit('The upload failed with an unexpected response: %s' % response)
-    except HttpError, e:
+    except HttpError as e:
       if e.resp.status in RETRIABLE_STATUS_CODES:
         error = 'A retriable HTTP error %d occurred:\n%s' % (e.resp.status,
                                                              e.content)
       else:
         raise
-    except RETRIABLE_EXCEPTIONS, e:
+    except RETRIABLE_EXCEPTIONS as e:
       error = 'A retriable error occurred: %s' % e
 
     if error is not None:
-      print error
+      print(error)
       retry += 1
       if retry > MAX_RETRIES:
         exit('No longer attempting to retry.')
 
       max_sleep = 2 ** retry
       sleep_seconds = random.random() * max_sleep
-      print 'Sleeping %f seconds and then retrying...' % sleep_seconds
+      print('Sleeping %f seconds and then retrying...' % sleep_seconds)
       time.sleep(sleep_seconds)
 
 if __name__ == '__main__':
   youtube = get_authenticated_service()
 
-  MEDIA_FOLDER_PATH = raw_input('Enter VIDEOS FOLDER PATH you wish to upload:\n')
+  MEDIA_FOLDER_PATH = input('Enter VIDEOS FOLDER PATH you wish to upload:\n')
   for video_file in glob.glob(os.path.join(MEDIA_FOLDER_PATH, '*.*')):
     mimetypes.init()
     mimestart = mimetypes.guess_type(video_file)[0]
@@ -123,6 +123,7 @@ if __name__ == '__main__':
         args = parser.parse_args()
         try:
           initialize_upload(youtube, args, video_file)
-        except HttpError, e:
-          print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
+        except HttpError as e:
+          print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
               
+
